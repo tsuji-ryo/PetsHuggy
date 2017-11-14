00a4700af7459225b283c2c1363f5b0711540717
tsuji <>
2017-11-14 11:43:43 +0900

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  root :to => 'pages#index'
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+
+  resources :users, only: [:show]
+end
